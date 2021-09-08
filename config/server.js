607d2e3bf91003da2c6ab5edaac1925d62e84528
Roger Selwan <roger@fusionsecond.com>
2021-09-08 15:55:20 +0300

    Backend for dj-events

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e45d54f006ed4ed77492a776f76b4ff7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'acd221f6-b31f-4aaa-98b8-2eae260ba051'
+};
